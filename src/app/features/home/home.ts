6a26b145d6e8158a14251b9a85f15483c73fdb6d
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-27 22:58:40 -0300

    feat: add home balance cards

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { BalanceCard } from './components/balance-card/balance-card';
+import { BalanceCardData } from './models/balance-card.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatGridListModule, BalanceCard],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {}
+export class Home {
+  protected readonly balanceData = signal<BalanceCardData[]>([
+    { header: 'Income', amount: 5000, type: 'income' },
+    { header: 'Expenses', amount: 2000, type: 'expenses' },
+    { header: 'Balance', amount: 2003000, type: 'balance' }
+  ]);
+}
