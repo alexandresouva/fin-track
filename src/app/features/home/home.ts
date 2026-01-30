9284d3bc93e7594116eb481d653e30df32b8cc7b
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-29 22:40:29 -0300

    feat: integrate transaction list in home component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,19 +2,37 @@ import { Component, signal } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 import { Balance } from './components/balance/balance';
+import { TransactionList } from './components/transaction-list/transaction-list';
 import { Transaction } from './models/transaction.model';
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, Balance],
+  imports: [MatGridListModule, Balance, TransactionList],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  protected readonly transactions = signal<Transaction[]>([
-    { type: 'income', value: 1000 },
-    { type: 'expenses', value: 500 },
-    { type: 'expenses', value: 2000 },
-    { type: 'income', value: 300 }
+  readonly transactions = signal<Transaction[]>([
+    {
+      id: '1',
+      title: 'Salary',
+      date: '2025-01-10',
+      value: 3000,
+      type: 'income'
+    },
+    {
+      id: '2',
+      title: 'Rent',
+      date: '2025-01-12',
+      value: 1200,
+      type: 'expenses'
+    },
+    {
+      id: '3',
+      title: 'Groceries',
+      date: '2025-01-15',
+      value: 300,
+      type: 'expenses'
+    }
   ]);
 }
